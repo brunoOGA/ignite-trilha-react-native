dad2d52cfef83f4909dfa2315ad4b98bb2b99c9c
Bruno Arruda <bruno.arruda@vexpenses.com>
2021-10-04 08:21:44 -0300

    Chapter 3: Consumindo API - Criação das Interfaces

--- Chapter 3/rentx/App.tsx
+++ Chapter 3/rentx/App.tsx
@@ -1,7 +1,38 @@
+import 'react-native-gesture-handler';
+import React from "react";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 
-import React from 'react';
-import { Home } from './src/screens/Home';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/styles/theme";
+import Routes from './src/routes';
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
 }
---
+++ Chapter 3/rentx/src/screens/Scheduling/index.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "styled-components";
+import BackButton from "../../components/BackButton";
+import ArrowSvg from "../../assets/arrow.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from "./styles";
+import { StatusBar } from "react-native";
+import Button from "../../components/Button";
+import Calendar from "../../components/Calendar";
+
+const Scheduling: React.FC = () => {
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  function handleConfirmRental() {
+    navigation.navigate("SchedulingDetails");
+  }
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={handleGoBack} color={theme.colors.shape} />
+        <Title>
+          Escolha uma {"\n"}data de início e {"\n"}fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>16/03/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>16/03/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Calendar />
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" onPress={handleConfirmRental} />
+      </Footer>
+    </Container>
+  );
+};
+
+export default Scheduling;
